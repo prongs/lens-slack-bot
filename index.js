9a5372c82fc94356a44955e03f541926d7377ec3
Rajat Khandelwal <rajatgupta59@gmail.com>
2016-03-02 20:15:02 +0530

    Added package.json

--- index.js
+++ index.js
@@ -3,42 +3,47 @@ var Botkit = require('botkit');
 var LensClient = require("lens-node-client");
 var YAML = require("yamljs");
 
-nconf.argv()
-    .env()
-    .file({ file: nconf.get("HOME") + '/lens-slack-bot-config.json' })
-    .file({ file: 'lens-slack-bot-config.json' });
+function LensSlackBot() {
+    nconf.argv()
+        .env()
+        .file({file: nconf.get("HOME") + '/lens-slack-bot-config.json'})
+        .file({file: 'lens-slack-bot-config.json'});
 
-var slackToken = nconf.get("slackToken");
-var client = new LensClient({
-    lensServerBaseUrl: nconf.get("lensServerBaseUrl"),
-    username: nconf.get("lensUsername"),
-    password: nconf.get("lensPassword")
-});
-var handleRegexString = "\\w{8}-(\\w{4}-){3}\\w{12}";
-var controller = Botkit.slackbot({});
-controller.spawn({token: slackToken}).startRTM();
+    var slackToken = nconf.get("slackToken");
+    var client = new LensClient({
+        lensServerBaseUrl: nconf.get("lensServerBaseUrl"),
+        username: nconf.get("lensUsername"),
+        password: nconf.get("lensPassword")
+    });
+    var handleRegexString = "\\w{8}-(\\w{4}-){3}\\w{12}";
+    var controller = Botkit.slackbot({});
+    controller.spawn({token: slackToken}).startRTM();
 
-function forEachHandle(text, callback) {
-    text.match(/\w{8}-(\w{4}-){3}\w{12}/g).forEach(callback);
+    function forEachHandle(text, callback) {
+        text.match(/\w{8}-(\w{4}-){3}\w{12}/g).forEach(callback);
+    }
+
+    this.start = function () {
+        controller.hears(["details", "query details"], ['direct_message', 'direct_mention', 'mention', 'ambient'],
+            function (bot, message) {
+                forEachHandle(message.text, function (handle) {
+                    client.getQuery(handle, function (lensQuery) {
+                        bot.reply(message, "`" + handle + "`:\n```" + YAML.stringify(lensQuery) + "```");
+                    });
+                });
+            }
+        );
+
+        controller.hears(["status", "query status"],
+            ['direct_message', 'direct_mention', 'mention', 'ambient'],
+            function (bot, message) {
+                forEachHandle(message.text, function (handle) {
+                    client.getQuery(handle, function (lensQuery) {
+                        bot.reply(message, "`" + handle + "`:\n```" + YAML.stringify(lensQuery.status) + "```");
+                    });
+                });
+            }
+        );
+    }
 }
-
-controller.hears(["details", "query details"], ['direct_message', 'direct_mention', 'mention', 'ambient'],
-    function (bot, message) {
-        forEachHandle(message.text, function (handle) {
-            client.getQuery(handle, function (lensQuery) {
-                bot.reply(message, "`" + handle + "`:\n```" + YAML.stringify(lensQuery) + "```");
-            });
-        });
-    }
-);
-
-controller.hears(["status", "query status"],
-    ['direct_message', 'direct_mention', 'mention', 'ambient'],
-    function (bot, message) {
-        forEachHandle(message.text, function (handle) {
-            client.getQuery(handle, function (lensQuery) {
-                bot.reply(message, "`" + handle + "`:\n```" + YAML.stringify(lensQuery.status) + "```");
-            });
-        });
-    }
-);
+module.exports = LensSlackBot;
