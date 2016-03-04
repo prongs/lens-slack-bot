d6a61086e046140a187a1c2cc7aa4476e7caadf8
Rajat Khandelwal <rajatgupta59@gmail.com>
2016-03-04 15:53:20 +0530

    more intuitive chat

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function LensSlackBot() {
         username: nconf.get("lensUsername"),
         password: nconf.get("lensPassword")
     });
-    var handleRegexString = "\\w{8}-(\\w{4}-){3}\\w{12}";
+    var handleRegexString = "\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12}";
+    var handleRegex = new RegExp(handleRegexString, "g");
 
     function getMessage(heading, details) {
         if (!details) {
@@ -45,58 +46,70 @@ function LensSlackBot() {
                 bot.say({type: "ping", id: now.getTime()}, updateLastActiveTime);
             }
         });
-        controller.hears(["^\\s*(\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12})\\s*:\\s*(.*?)\\s*$"],
-            ['direct_message', 'direct_mention', 'mention', 'ambient'],
-            function (bot, message) {
-                var handle = message.match[1];
-                var fields = message.match[2].split(/\s*,\s*/);
-                for (var i = 0; i < fields.length; i++) {
-                    fields[i] = fields[i].replace(/\s(.)/g, function (x) {
-                        return x.toUpperCase().trim();
-                    }).replace(/^(.)/, function (x) {
-                        return x.toLowerCase();
+        function parseFields(str) {
+            if (!str) {
+                return null;
+            }
+            var fields = str.trim().split(/\s*,\s*/);
+            for (var i = 0; i < fields.length; i++) {
+                fields[i] = fields[i].replace(/\s(.)/g, function (x) {
+                    return x.toUpperCase().trim();
+                }).replace(/^(.)/, function (x) {
+                    return x.toLowerCase();
+                });
+            }
+            return fields;
+        }
+
+        function respondWithDetails(message, handles, fields) {
+            function reply(heading, details) {
+                var reply = getMessage(heading, details);
+                if (reply.length < 4000) {
+                    bot.reply(message, reply, updateLastActiveTime);
+                } else {
+                    if (typeof(details) == "object") {
+                        details = YAML.stringify(details);
+                    }
+                    bot.api.files.upload({
+                        content: details,
+                        filetype: 'yaml',
+                        title: heading,
+                        filename: heading,
+                        channels: message.channel
+                    }, function (error, json) {
+                        console.log(error);
+                        console.log(json);
                     });
                 }
-                function reply(heading, details) {
-                    var reply = getMessage(heading, details);
-                    if (reply.length < 4000) {
-                        bot.reply(message, reply, updateLastActiveTime);
-                    } else {
-                        bot.api.files.upload({
-                            content: details,
-                            filetype: 'yaml',
-                            title: heading,
-                            filename: heading,
-                            channels: message.channel
-                        }, function (error, json) {
-                            console.log(error);
-                            console.log(json);
-                        });
-                    }
-                }
+            }
 
-                function sendQueryDetails(query) {
-                    if (fields.length == 1) {
-                        if (fields[0] in query) {
-                            reply(fields[0] + " for " + query.queryHandle.handleId, query[fields[0]]);
-                        } else {
-                            reply("No field named " + fields[0] + " in query " + query.queryHandle.handleId);
-                        }
+            function sendQueryDetails(query) {
+                if (fields && fields.length == 1) {
+                    if (fields[0] in query) {
+                        reply(fields[0] + " for " + query.queryHandle.handleId, query[fields[0]]);
                     } else {
-                        var data = {};
+                        reply("No field named " + fields[0] + " in query " + query.queryHandle.handleId);
+                    }
+                } else {
+                    var data = {};
+                    if (fields) {
                         fields.forEach(function (fieldName) {
                             if (fieldName in query) {
                                 data[fieldName] = query[fieldName];
                             }
                         });
-                        if (data) {
-                            reply(query.queryHandle.handleId, data);
-                        } else {
-                            reply("No data found for " + query.queryHandle.handleId);
-                        }
+                    } else {
+                        data = query;
+                    }
+                    if (data) {
+                        reply(query.queryHandle.handleId, data);
+                    } else {
+                        reply("No data found for " + query.queryHandle.handleId);
                     }
                 }
+            }
 
+            handles.forEach(function (handle) {
                 queryCache.get(handle, function (error, value) {
                     if (!error) {
                         if (!value) {
@@ -109,31 +122,47 @@ function LensSlackBot() {
                         }
                     }
                 });
-            }
-        );
-        controller.hears([".*?queries.*"],
+            });
+        }
+
+        controller.hears(["^(((" + handleRegexString + ")\\s*,?\\s*)+)(:(.*?))?$"],
             ['direct_message', 'direct_mention', 'mention', 'ambient'],
             function (bot, message) {
-                var matches = message.text.match(/(\w+)=`(.*?)`/g);
+                var handles = message.match[1].match(handleRegex);
+                var fields = parseFields(message.match[5]);
+                respondWithDetails(message, handles, fields);
+            }
+        );
+        controller.hears(["(all\\s*)?(\\w+)\\squeries(.*?)(:\\s*(.*?))?$"],
+            ['direct_message', 'direct_mention', 'mention', 'ambient'],
+            function (bot, message) {
                 var qs = {};
-                var queryParams = [];
-                for (var i in matches) {
-                    var kv = matches[i].split("=");
-                    qs[kv[0]] = kv[1].replace(/`/g, "");
+                if (message.match[3]) {
+                    var params = message.match[3].trim().split(/[^a-zA-Z.]+/);
+                    if (params.length % 2 != 0) {
+                        bot.reply("Sorry couldn't parse your arguments: " + message.match[3], updateLastActiveTime);
+                        return;
+                    } else {
+                        for (var i = 0; i < params.length; i += 2) {
+                            qs[params[i]] = params[i + 1];
+                        }
+                    }
                 }
                 if (!('state' in qs)) {
-                    var split = message.text.split("queries");
-                    if (split.length > 1) {
-                        qs['state'] = split[0].replace(/all/g, '');
+                    if (message.match[2]) {
+                        qs['state'] = message.match[2]
                     }
                 }
                 if ('state' in qs) {
                     qs['state'] = qs['state'].toUpperCase().trim();
                 }
                 if (!('user' in qs)) {
-                    if (message.text.indexOf("all") > -1) {
+                    if (message.match[1] || message.text.indexOf("all") > -1) {
                         qs['user'] = 'all';
                     }
+                }
+                if (message.match[5]) {
+                    var fields = parseFields(message.match[5]);
                 }
                 function reply(queries) {
                     var reply = "`" + JSON.stringify(qs) + "`(" + queries.length + " )";
@@ -143,7 +172,15 @@ function LensSlackBot() {
                     if (queries.length > 0) {
                         reply = reply + "\n```" + YAML.stringify(queries) + "```";
                     }
-                    bot.reply(message, reply, updateLastActiveTime);
+                    bot.reply(message, reply, function () {
+                        updateLastActiveTime();
+                        if (fields) {
+                            if (fields.length == 1 && fields[0].match(/details/i)) {
+                                fields = null;
+                            }
+                            respondWithDetails(message, queries, fields);
+                        }
+                    });
                 }
 
                 if (!('user' in qs)) {
