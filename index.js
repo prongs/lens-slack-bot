f155c7510a60f9ba2b7723fa9107c3f2eb368652
Rajat Khandelwal <rajatgupta59@gmail.com>
2016-07-19 15:42:55 +0530

    more than 24 hours looks like 0 hours

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ alasql.fn.milliseconds_to_time = (duration) => {
   let milliseconds = parseInt((duration % 1000) / 100);
   let seconds = parseInt((duration / 1000) % 60);
   let minutes = parseInt((duration / (1000 * 60)) % 60);
-  let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+  let hours = parseInt(duration / (1000 * 60 * 60));
   hours = (hours < 10) ? "0" + hours : hours;
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
